435242ea5dd13077acfeb301aa348c8be1cf438b
charliecollyer <charliecollyer@icloud.com>
2025-12-29 21:55:46 +0000

    add daisyui icons and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  css: ["./app/assets/css/main.css"],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
